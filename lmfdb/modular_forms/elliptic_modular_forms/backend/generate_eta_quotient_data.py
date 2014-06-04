67d30174e6f8d6d2660810aa11d382f1f6b58cbd
Sebastian Opitz <opitz@mathematik.tu-darmstadt.de>
2014-06-04 11:24:44 +0100

    worked work on the eta products

--- lmfdb/modular_forms/elliptic_modular_forms/backend/generate_eta_quotient_data.py
+++ lmfdb/modular_forms/elliptic_modular_forms/backend/generate_eta_quotient_data.py
@@ -4,22 +4,32 @@ from dirichlet_conrey import DirichletGroup_conrey
 # from lmfdb.website import dbport
 from sage.all import ZZ, QQ
 from lmfdb.modular_forms.elliptic_modular_forms.backend.web_modforms import WebModFormSpace
+import itertools
+for element in itertools.product(*somelists):
 
 def generate_eta_quotient_data(maxN=100, maxk=12):
     data = dict()
     for N in range(1, maxN):
         data[N] = dict()
-        for k in range(2, maxk):
-            for chiorbit in [chi for chi in range ]
-            W = WebModFormSpace(N=int(N), k=int(k), chi=int(chi))
-            finds = ms.find({'t': [int(N), int(k), int(0)]})
-            print finds.count()
-            in_db = finds.count() > 0
-            data[N][k] = {'dimension': dim, 'in_db': in_db}
-            # print N,k,data[N][k]
+        deltas = N.divisors()
+        delta_n = len(deltas)
+        for rdeltas in itertools.product(range(0,2*maxk+1), repeat=delta_n):
+            k = Integer(sum(rdeltas))
+            if k % 2 = 0 and k <= 2*maxk:
+                if sum([rdeltas[j]*deltas[j]] for j in range(0,)) % 24 == 0:
+                    if N * sum([Integer(rdeltas[j])/Integer(deltas[j])] for j in range(0,)) % 24 == 0:
+                        k = k // 2
+
+
+
+                
+                W = WebModFormSpace(N=int(N), k=int(k), chi=int(chi))
+                
+                
+                data[N][k] = {'dimension': dim, 'in_db': in_db}
             
     
-    return ms, data
+    return data
 
 def eta_quotient_with_conrey_number(A, N, arguments, exponents):
     r"""
@@ -35,7 +45,7 @@ def eta_quotient_with_conrey_number(A, N, arguments, exponents):
     
     if N % 4 == 0:
         c1 = kronecker_character(-1)**(k+((kronecker(-1,A)-1)//2))
-        c2 = kronecker_character(2**(2k) * A)
+        c2 = kronecker_character(2**(2*k) * A)
         chi = D(c1)*D(c2)
     else:
         chi = kronecker_character(A)
